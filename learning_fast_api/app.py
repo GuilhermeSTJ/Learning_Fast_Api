0537ce0de5e98b3d87a0cace57ae5b3559aba260
Guilherme Teodoro | Tendencias Consultoria <gteodoro@tendencias.com.br>
2024-06-26 15:50:42 -0300

    Avanços nos estudo da fastApi, entendimento dos schemas do pydantic e testes

--- learning_fast_api/app.py
+++ learning_fast_api/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from learning_fast_api.schemas import Message, User, UserDB, UserPublic
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: User):
+    user_with_id = UserDB(
+        id=len(database) + 1,
+        **user.model_dump()
+    )
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,36 @@
 from http import HTTPStatus
 
+import pytest
+
 from fastapi.testclient import TestClient
 
 from learning_fast_api.app import app
 
+@pytest.fixture()
+def client():
+    return TestClient(app)
 
-def test_read_root_return_ok_and_hW():
-    client = TestClient(app)  # Arrange
+def test_read_root_return_ok_and_helloWord(client):
 
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
 
-    assert response.json() == {'message': 'Hello, World!'}  # Assert    
+    assert response.json() == {'message': 'Hello, World!'}  # Assert
+
+
+def test_create_user(client):
+
+    response = client.post('/users', json={
+        'username': 'test',
+        'email': 'test@test.com',
+        'password': 'test1234'
+    })  # Act
+
+    assert response.status_code == HTTPStatus.CREATED  # Assert
+
+    assert response.json() == {
+        'username': 'test',
+        'email': 'test@test.com',
+        'id': 1
+    }  # Assert
